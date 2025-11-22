958fea7554f28aff3860f4452748a9f73438b145
LsMin124 <xleex0124@gmail.com>
2025-11-23 01:42:48 +0900

    feat: Task 엔티티 완성

--- src/main/java/com/example/demo/domain/Task.java
+++ src/main/java/com/example/demo/domain/Task.java
@@ -1,11 +1,13 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Auditable;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tasks")
@@ -13,4 +15,34 @@ import org.springframework.data.domain.Auditable;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Task extends BaseTimeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskType taskType;
+
+    private LocalDate dueDate;
+
+    private String recurrenceRule;
+
+    @Builder
+    public Task(User user,  String title, String description, TaskType taskType, LocalDate dueDate, String recurrenceRule) {
+        this.user = user;
+        this.title = title;
+        this.description = description;
+        this.taskType = taskType;
+        this.dueDate = dueDate;
+        this.recurrenceRule = recurrenceRule;
+    }
 }
