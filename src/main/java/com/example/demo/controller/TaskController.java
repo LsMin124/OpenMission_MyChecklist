6b7262bfc6a661c2b35f6a7fe24b8cba9ac2bc2a
LsMin124 <xleex0124@gmail.com>
2025-11-24 15:21:36 +0900

    chore: 테스트용 하드코딩 사용자id 제거 및 토큰 인증 기능 적용

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -19,40 +21,48 @@ public class TaskController {
 
     private final TaskService taskService;
 
-    // 추후 인증기능 추가 전까진 테스트용 id 사용
-    private static final Long TEMP_USER_ID =1L;
-
     // Task 등록
     @PostMapping
-    public ResponseEntity<Long> createTask(@RequestBody @Valid TaskRequest request) {
-        Long taskId = taskService.createTask(TEMP_USER_ID, request);
+    public ResponseEntity<Long> createTask(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody @Valid TaskRequest request) {
+
+        Long userId = Long.parseLong(userDetails.getUsername());
+
+        Long taskId = taskService.createTask(userId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskId);
     }
 
     // 오늘 task 목록 조회
     @GetMapping("/today")
     public ResponseEntity<List<TaskResponse>> getTasksForToday(
+            @AuthenticationPrincipal UserDetails userDetails,
             @RequestParam(required = false) LocalDate date) {
+
+        Long userId = Long.parseLong(userDetails.getUsername());
 
         if (date == null) {
             date = LocalDate.now();
         }
 
-        List<TaskResponse> responses = taskService.getTasksForToday(TEMP_USER_ID, date);
+        List<TaskResponse> responses = taskService.getTasksForToday(userId, date);
         return ResponseEntity.ok(responses);
     }
 
     // 작업 완료 처리
     @PostMapping("/{taskId}/complete")
     public ResponseEntity<Long> completeTask(
+            @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable Long taskId,
             @RequestParam(required = false) LocalDate date) {
+
+        Long userId = Long.parseLong(userDetails.getUsername());
 
         if (date == null) {
             date = LocalDate.now();
         }
 
-        taskService.completeTask(TEMP_USER_ID, taskId, date);
+        taskService.completeTask(userId, taskId, date);
         return ResponseEntity.ok().build();
     }
 
